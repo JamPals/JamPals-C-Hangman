0675f4cd5a60988e82a937822d41ab84969fa684
Orixinz <151961675+JamPals@users.noreply.github.com>
2024-04-20 14:06:51 +0100

    Fix commenting in main.c
    Code just decided to go odd for some reason, commenting out very important lines of code, including the #include statements.

--- main.c
+++ main.c
@@ -1,10 +1,21 @@
-// Hangman Game by JamPals// This was built by me to learn C. I might add a graphics libary to this at some point as I think it would be fun, but it might be a headache.#include <stdio.h>#include <stdlib.h>#include <time.h>// The stages of ASCII art used to draw the hangman to the screen.char * stage1 = "---\n"; 
+// Hangman Game by JamPals
+// This was built by me to learn C. I might add a graphics libary to this at some point as I think it would be fun, but it might be a headache.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+// The stages of ASCII art used to draw the hangman to the screen.
+char * stage1 = "---\n"; 
 char * stage2 = " | \n";
 char * stage3 = " | \n";
 char * stage4 = " ____\n";
 char * stage3re = " |  |\n";
 char * stage2re = " |(*_*)\n";
-// The words that the game can pick from.char* words[] = {"Hello", "Tree", "House", "Computer"};
+
+// The words that the game can pick from.
+char* words[] = {"Hello", "Tree", "House", "Computer"};
+
 // Print Stage Function
 void printStage(int stage) {
     if(stage == 1) {
@@ -44,9 +55,17 @@ char charInput(char inputText[]) {
     scanf("%c", &input);  
     return input;
 }
+
 int main() {
     time_t t;    
-    srand((unsigned) time(&t)); // Initalises the random number generator    printf("Welcome to hangman, lets get started.\n");
-    // Word Gen     char * word = words[rand() % 4]; 
-    printf("%s\n", word); // TODO REMOVE    printf("%c \n", charInput("Enter your first letter: ")); // TODO REMOVE    return 0;
+    srand((unsigned) time(&t)); // Initalises the random number generator    
+    printf("Welcome to hangman, lets get started.\n");
+
+    // Word Gen     
+    char * word = words[rand() % 4]; 
+    printf("%s\n", word); // TODO REMOVE    
+
+    printf("%c \n", charInput("Enter your first letter: ")); // TODO REMOVE    
+
+    return 0;
 }
